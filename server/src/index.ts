f0891a9a87d622ebff769720dcc1ff9943261483
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-07-30 18:13:53 +0100

    Moved the GraphQL resolvers into its own folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,14 @@
 import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import dotenv from 'dotenv';
-import typeDefs from './typeDefs/index';
+import typeDefs from './typeDefs';
+import resolvers from './resolvers';
 
 // Instantiate Express
 const app = express();
 
 // Set up DotEnv
 dotenv.config();
-
-// Define Resolvers
-const resolvers = {
-  Query: {
-    hello: () => 'Hello World'
-  }
-};
 
 // Check Node Environment in order to activate GRAPHQL playground
 const IS_DEV = process.env.NODE_ENV === 'development';
@@ -29,9 +23,10 @@ const server = new ApolloServer({
 // Apply GraphQL Middleware to Express App
 server.applyMiddleware({ app });
 
-const PORT = process.env.PORT || 4000;
+// Set Port Number
+const { PORT } = process.env;
 
 // Start GraphQL Server
-app.listen(PORT, () =>
+app.listen(PORT || 4000, () =>
   console.log(`Server ready at http://localhost:${PORT}/graphql`)
 );
---
+++ server/src/resolvers/hello.ts
@@ -0,0 +1,8 @@
+// Define Resolvers
+const helloResolver = {
+  Query: {
+    hello: () => 'Hello World'
+  }
+};
+
+export default helloResolver;
