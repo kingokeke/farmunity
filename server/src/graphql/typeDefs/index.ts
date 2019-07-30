6909e83ece3d3c6b0c58559cc0b7b336ee6368e2
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-07-30 22:59:07 +0100

    Added type schema for addUser mutation

--- server/src/graphql/typeDefs/index.ts
+++ server/src/graphql/typeDefs/index.ts
@@ -1,4 +1,5 @@
 import root from './root';
 import hello from './hello';
+import user from './user';
 
-export default [root, hello];
+export default [root, hello, user];
---
+++ server/src/graphql/typeDefs/user.ts
@@ -0,0 +1,15 @@
+import { gql } from 'apollo-server-express';
+
+// Define Type Schema
+const userTypeDefs = gql`
+  extend type Mutation {
+    addUser(firstName: String!): User
+  }
+
+  type User {
+    id: ID!
+    firstName: String
+  }
+`;
+
+export default userTypeDefs;
