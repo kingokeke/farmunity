be90bfe3ca4c32086ec52170bbdf8c182dc4652c
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-07-31 18:51:48 +0100

    Added remaining fields for addUser type definitions

--- server/src/graphql/typeDefs/userTypeDefs.ts
+++ server/src/graphql/typeDefs/userTypeDefs.ts
@@ -5,12 +5,32 @@ import { gql } from 'apollo-server-express';
 // Define Type Schema
 const userTypeDefs = gql`
   extend type Mutation {
-    addUser(firstName: String!, password: String!): User
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      phone: String!
+      password: String!
+      role: String!
+      streetAddress: String
+      localGovt: String
+      state: String
+      profilePic: String
+    ): User
   }
 
   type User {
     id: ID!
     firstName: String!
+    lastName: String!
+    email: String!
+    phone: String!
+    password: String!
+    role: String!
+    streetAddress: String
+    localGovt: String
+    state: String
+    profilePic: String
   }
 `;
 
