51cc3d348dd70cd862c5f1200c9c70106d0026a5
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-07-31 17:04:29 +0100

    Added Joi validation to addUser mutation

--- server/src/graphql/resolvers/userResolvers.ts
+++ server/src/graphql/resolvers/userResolvers.ts
@@ -1,18 +1,31 @@
 // RESOLVER FUNCTIONS FOR USER RESOURCE
 
 import { User as UserType } from '../../typings/index';
+import Joi from '@hapi/joi';
 import { User } from '../../models';
+import { addUserSchema } from '../../validation/';
 
 // Define Resolvers
 const userResolver = {
   Mutation: {
     addUser: async (_root: any, args: UserType) => {
-      const newUser = new User({
-        firstName: args.firstName,
-        password: args.password
-      });
-      const { id, firstName } = await newUser.save();
-      return { id, firstName };
+      try {
+        const value = await Joi.validate(args, addUserSchema, {
+          abortEarly: false
+        });
+
+        const newUser = new User({
+          firstName: value.firstName,
+          password: value.password
+        });
+
+        const { id, firstName } = await newUser.save();
+
+        return { id, firstName };
+      } catch (err) {
+        console.log(err);
+        return;
+      }
     }
   }
 };
---
+++ server/src/validation/index.ts
@@ -0,0 +1,1 @@
+export { default as addUserSchema } from './addUserSchema';
